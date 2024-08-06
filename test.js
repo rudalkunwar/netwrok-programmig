10c8bd39664f993c95b4c077b793ebce63b7b9f3
rudal kunwar <ruzalkunwar@gmail.com>
2024-08-06 15:53:13 +0545

    fixes;

--- test.js
+++ test.js
@@ -6,3 +6,7 @@ hello();
 function notHello(){
     console.log('javascript everywere ');
 }
+
+const Fixpc = ( ) =>{
+    console.log("dones this fix the pc ore what i think i am not getting any errors now ");
+}
