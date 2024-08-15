ea7c363e3b3035d7116d555ed400844272d39411
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-08-15 18:11:39 +0545

    mac fixed;

--- exam/MacAddress.java
+++ exam/MacAddress.java
@@ -1,23 +1,18 @@
 import java.net.*;
+public class MacAddress{
+    public static void main(String[] args) throws UnknownHostException,SocketException{
 
-public class SimpleMacAddress {
-    public static void main(String[] args) throws Exception {
+        InetAddress local = InetAddress.getLocalHost();
+        NetworkInterface ni = NetworkInterface.getByInetAddress(local);
 
-        InetAddress localHost = InetAddress.getLocalHost();
+        byte[] mac = ni.getHardwareAddress();
+        if(mac!=null){
+            System.out.print("The mac address is: ");
+           for(int i=0;i<mac.length;i++){
 
-        NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
-
-        byte[] mac = networkInterface.getHardwareAddress();
-
-        if (mac != null) {
-            System.out.println("Mac address is ");
-            
-            for (int i = 0; i < mac.length; i++) {
-            System.out.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : "");
-            }
-
-        } else {
-            System.out.println("MAC Address not available.");
+            System.out.format("%02X%s",mac[i],(i<mac.length-1)?"-":"");
+           }
+           System.out.println();
         }
     }
 }
