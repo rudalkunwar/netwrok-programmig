9f75df784e7aaf3159a9ffcf8085a9e4ff845751
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-08-20 17:34:11 +0545

    client socket

---
+++ exam/ClientSocket.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+public class ClientSocket{
+    public static void main(String[] args)throws IOException{
+
+        String host = "time.nist.gov";
+
+        Socket s = new Socket(host,13);
+
+
+
+        System.out.println("Local address "+s.getLocalAddress());
+        System.out.println("Local port "+s.getLocalPort());
+
+        System.out.println("Remote address "+s.getInetAddress());
+        System.out.println("Remote port "+s.getPort());
+
+
+        InputStream is = s.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        br.lines().forEach(System.out::println);
+        System.out.println(new Date().toString());
+        // int line;
+        // while(line=br.read()!=-1){
+        //     System.out.println((char)line);
+        // }
+    }
+}
